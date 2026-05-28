0f1612ae71e6e4bb831e1c27f5e0b159a10fe9e4
seeker <83727884+seekehr@users.noreply.github.com>
2026-05-28 20:13:59 +0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7DE71F8YY6"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7DE71F8YY6');
+            `,
+          }}
+        />
+      </head>
       <body className="font-sans antialiased">
         <Script
           id="json-ld"
